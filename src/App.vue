483d038cbf8388e3bfae611d751ede98851bc9c8
Szymon Szymkowiak <szymkowiak.szymon@gmail.com>
2019-11-14 18:59:41 +0100

    prepare basic app views

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <v-app>
-    <v-content>
+  <v-app class="grey lighten-4">
+    <Navbar />
 
+    <v-content>
+      <router-view></router-view>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar'
 export default {
+  components: { Navbar },
   name: 'App',
+  data () {
+    return {
 
-
-
-  data: () => ({
-    //
-  }),
-};
+    }
+  }
+}
 </script>
