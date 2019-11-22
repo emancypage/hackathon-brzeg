63b96e54c4890bc189ecb9a72eab729457261aab
s_szymkowiak2 <szymon.szymkowiak@kiratik.de>
2019-11-22 17:43:27 +0100

    Fixed covered drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-app-bar app text>
+    <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer" class="grey--text"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Agro</span>
