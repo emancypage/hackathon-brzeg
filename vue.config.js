52df10a8cc10637cbc2a0910a0dc3840bda20372
s_szymkowiak2 <szymon.szymkowiak@kiratik.de>
2019-11-22 21:22:12 +0100

    Change icon

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,6 @@ module.exports = {
     name: 'AgroTech',
     themeColor: '#367c2b',
     msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    iconPaths: {
-      favicon72: 'img/icons/icon-72x72.png',
-      favicon96: 'img/icons/icon-96x96.png',
-      appleTouchIcon: 'img/icons/icon-152x152.png',
-      msTileImage: 'img/icons/icon-144x144.png'
-    },
     manifestOptions: {
       icons: [
         {
