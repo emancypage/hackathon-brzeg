d5a78090524176d865bcfa6d0c808afcd1421b4e
Szymon Szymkowiak <szymkowiak.szymon@gmail.com>
2019-11-23 17:28:20 +0100

    Add link to b2b

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="about">
     <h1>O aplikacji</h1>
-    <p>{{ test }}</p>
+    <p>Aplikacja stworzona na potrzeby Hackathonu 2019 AI w Rolnictwie. Strona organizatora: <a href="https://code4poland.pl/">https://code4poland.pl/</a></p>
+
+    <p>Twórcy:</p>
+    <ul>
+      <li>Jakub Wojciechowski</li>
+      <li>Waldemar Stańko</li>
+      <li>Mateusz Mierzyński</li>
+      <li>Szymon Szymkowiak</li>
+    </ul>
   </div>
 </template>
 
 <script>
   export default {
     name: "About",
-    computed: {
-      test() {
-        return this.$store.state.test;
-      }
-    }
+
   }
 </script>
